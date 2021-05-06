33f0e455a728465e7c63be6b193e35f2bd08fd27
DENGARDEN <pooowooho3@gmail.com>
2021-05-06 22:34:44 +0900

    add js file

---
+++ backendLibrary.js
@@ -0,0 +1,12 @@
+//1. Retrieve data upon start using folders
+// use localstorage
+
+//2. Video file import button
+
+//3. Summary/ Full-text change button
+
+//4. Display the text that matches the type of current state (Summary / Full-text)
+
+//5. Class add/delete function
+
+//6. Program exit function
